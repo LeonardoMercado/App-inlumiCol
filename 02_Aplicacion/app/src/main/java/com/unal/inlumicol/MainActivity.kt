f0750ed69f159687df9d7daafbbd61b9f649f29e
Leonardo F. Mercado Benitez <lfmercadobe@unal.edu.co>
2021-11-15 22:22:07 -0500

    Se genera el viewBinding para el activity Main

--- 02_Aplicacion/app/src/main/java/com/unal/inlumicol/MainActivity.kt
+++ 02_Aplicacion/app/src/main/java/com/unal/inlumicol/MainActivity.kt
@@ -2,10 +2,20 @@ package com.unal.inlumicol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unal.inlumicol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.textTest.text = "Probando el View Binding"
+
+
     }
 }
