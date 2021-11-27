b6a1ce9262269aeefe5d84920bb7e657eb3e2c1d
Leonardo F. Mercado Benitez <lfmercadobe@unal.edu.co>
2021-11-27 10:46:42 -0500

    Se implementa el spinner de los municipios

--- 02_Aplicacion/app/src/main/java/com/unal/inlumicol/fragments/RegistroFallasFragment.kt
+++ 02_Aplicacion/app/src/main/java/com/unal/inlumicol/fragments/RegistroFallasFragment.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.unal.inlumicol.R
 import com.unal.inlumicol.databinding.FragmentRegistroFallasBinding
 
 class RegistroFallasFragment : Fragment(){
-
+    // For binding -----------------------------------------------------------
     private var _binding : FragmentRegistroFallasBinding? = null
     private val binding get() = _binding!!
+    //------------------------------------------------------------------------
+
+    // Variables -------------------------------------------------------------
+    private val municipiosList = listOf(
+        "Municipio",
+        "Soacha",
+        "Zipaquirá",
+        "Fusagasugá",
+        "Giradot",
+        "Facatativá",
+        "Tocaima",
+        "La Calera",
+        "Mosquera",
+        "Guatavita"
+    )
+    //------------------------------------------------------------------------
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,15 +38,22 @@ class RegistroFallasFragment : Fragment(){
         _binding = FragmentRegistroFallasBinding.inflate(inflater,container,false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO(Aqui código del registro de la falla)
-    }
 
+        // Llenar los spinners con datos:
+        llenarSpinners()
+    }
+    private fun llenarSpinners() {
+        // Para el spinner municipio
+        binding.spMunicipio.adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.spinner_item_personalised,
+            municipiosList
+        )
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
