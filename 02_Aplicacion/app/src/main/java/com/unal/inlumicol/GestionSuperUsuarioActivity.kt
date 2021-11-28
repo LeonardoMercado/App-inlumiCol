4b6e0f86831ad0e44f3482d73d666bcf54920599
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-28 02:42:29 -0500

    Se genera ventana gestion super usuario
    Solo se agregó los elementos de interfaz gráfica.

---
+++ 02_Aplicacion/app/src/main/java/com/unal/inlumicol/GestionSuperUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package com.unal.inlumicol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GestionSuperUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gestion_super_usuario)
+    }
+}
